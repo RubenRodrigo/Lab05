df9916fe28cc6da9f49666832f920e8e143f2c66
Ruben Rodrigo <rodrigohde905@gmail.com>
2021-04-13 20:05:05 -0500

    Suma de ejercicios

--- cursos/src/components/Content.js
+++ cursos/src/components/Content.js
@@ -2,13 +2,26 @@ import { Part } from "./Part"
 
 export const Content = ({parts}) => {
     
+    // Usamos esta funcion para devolver el atributo exercises del objeto en la iteracion.
+    const amount = (item) => {
+        return item.exercises
+    }
 
+    // Usamos esta función para devolver los valores prev y next sumados
+    const sum = (prev, next) => {        
+        return prev + next
+    }
+
+    const total = parts.map(amount).reduce(sum)
     return(
         <div>
             <div>
                 {
                     parts.map(part => <Part key={part.id} part={part} />)
                 }
+            </div>
+            <div>
+                <h5>La suma de ejercicios es: {total}</h5>
             </div>
         </div>
     )
